c6528411d96e4f77eae1c694aabe1ae3376d16ba
HarshilTomar20 <harshiltomar20@gmail.com>
2024-10-05 13:43:06 +0530

    feat: anchor account init

--- anchor-codebase/programs/anchor-codebase/src/lib.rs
+++ anchor-codebase/programs/anchor-codebase/src/lib.rs
@@ -1,16 +1,77 @@
 use anchor_lang::prelude::*;
 
-declare_id!("FzCA3TV2MnsHna8LwhW8HwyknW3oTDiWqUPGhfxyQPid");
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-pub mod anchor_codebase {
+pub mod invoice_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create_invoice(
+        ctx: Context<CreateInvoice>,
+        small_business: String,
+        large_business: String,
+        amount: u64,
+        invoice_number: String,
+        invoice_date: i64,
+        due_date: i64,
+        payment_terms: String,
+        small_business_address: String,
+        percentage_given: u8,
+        small_business_email: String,
+        lg_business_email: String,
+        status: String,
+    ) -> Result<()> {
+        let invoice = &mut ctx.accounts.invoice;
+        invoice.small_business = small_business;
+        invoice.large_business = large_business;
+        invoice.amount = amount;
+        invoice.invoice_number = invoice_number;
+        invoice.invoice_date = invoice_date;
+        invoice.due_date = due_date;
+        invoice.payment_terms = payment_terms;
+        invoice.small_business_address = small_business_address;
+        invoice.percentage_given = percentage_given;
+        invoice.small_business_email = small_business_email;
+        invoice.lg_business_email = lg_business_email;
+        invoice.status = status;
+        Ok(())
+    }
+
+    pub fn update_status(ctx: Context<UpdateStatus>, new_status: String) -> Result<()> {
+        let invoice = &mut ctx.accounts.invoice;
+        invoice.status = new_status;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateInvoice<'info> {
+    #[account(init, payer = user, space = 8 + 32 + 32 + 8 + 32 + 8 + 8 + 32 + 64 + 1 + 64 + 64 + 32)]
+    pub invoice: Account<'info, Invoice>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct UpdateStatus<'info> {
+    #[account(mut)]
+    pub invoice: Account<'info, Invoice>,
+    pub user: Signer<'info>,
+}
+
+#[account]
+pub struct Invoice {
+    pub small_business: String,
+    pub large_business: String,
+    pub amount: u64,
+    pub invoice_number: String,
+    pub invoice_date: i64,
+    pub due_date: i64,
+    pub payment_terms: String,
+    pub small_business_address: String,
+    pub percentage_given: u8,
+    pub small_business_email: String,
+    pub lg_business_email: String,
+    pub status: String,
+}
